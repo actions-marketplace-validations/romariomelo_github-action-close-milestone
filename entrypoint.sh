4f7aaa6f4ed8d01d9493b786c1f67b30111bb0e3
Romario Marques Melo <romario.melos@gmail.com>
2023-05-22 14:39:24 -0300

    fix formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,15 @@ NUMBER=$(curl -L -s \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/milestones?state=open | jq "map(select(.title == \"${INPUT_TITLE}\").number)[]")
   
 if [ $NUMBER ]; then
-	curl -L -s \
-  		-X PATCH \
-  		-H "Accept: application/vnd.github+json" \
-  		-H "Authorization: Bearer ${GITHUB_TOKEN}"\
-  		-H "X-GitHub-Api-Version: 2022-11-28" \
-  		https://api.github.com/repos/${GITHUB_REPOSITORY}/milestones/$NUMBER \
-  		-d '{"state":"closed"}'
+  curl -L -s \
+      -X PATCH \
+      -H "Accept: application/vnd.github+json" \
+      -H "Authorization: Bearer ${GITHUB_TOKEN}"\
+      -H "X-GitHub-Api-Version: 2022-11-28" \
+      https://api.github.com/repos/${GITHUB_REPOSITORY}/milestones/$NUMBER \
+      -d '{"state":"closed"}'
   
   echo "number=$NUMBER" >> $GITHUB_OUTPUT
 else 
-	echo "number=0" >> $GITHUB_OUTPUT
+  echo "number=0" >> $GITHUB_OUTPUT
 fi
